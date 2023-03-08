4928b170b8ac103789f9bdd19f02422db24a6865
SitthikornAimkhon <sitthikorn.aim@gmail.com>
2023-03-09 03:55:40 +0700

    Create Navbar import btn and export btn

--- data-visualize-webapp/src/App.js
+++ data-visualize-webapp/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+// You can specify which plugins you need here
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+// Import from components file
+import { NavbarBtn } from './components/NavbarBtn';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavbarBtn />
+      <Container>
+        <Row>
+          <Col>
+            <h1>1</h1>
+          </Col>
+          <Col>
+            <h1>2</h1>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
