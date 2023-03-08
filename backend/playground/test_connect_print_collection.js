d6b2ff6ca260ad5ec610271be0ab91490254a303
Aingthawawn K <109336369+aingthawan@users.noreply.github.com>
2023-03-08 23:49:13 +0700

    Update test_connect_print_collection.js
    add function to print all fields in a collection with data type

--- backend/playground/test_connect_print_collection.js
+++ backend/playground/test_connect_print_collection.js
@@ -1,5 +1,6 @@
 // A program for testing connection to MongoDB 
 // print all collections in all databases
+// print all fields in a collection with data type
 
 const {MongoClient} = require('mongodb');
 
@@ -16,11 +17,31 @@ async function listCollections(client, dbName){
     collectionsList = await client.db(dbName).listCollections().toArray();
     if (collectionsList.length > 0){
         console.log(` - ${dbName}`);
-        collectionsList.forEach(collection => console.log(`    - ${collection.name}`));
+        // pass to function to list all fields in a collection
+        for (const collection of collectionsList){
+            await listFields(client, dbName, collection.name);
+        }
     } else {
         console.log(` - ${dbName} has no collections`);
     }
 };
+
+// list all fields in a collection with data type
+async function listFields(client, dbName, collectionName){
+    const collection = client.db(dbName).collection(collectionName);
+    const cursor = collection.find();
+    const firstDoc = await cursor.next();
+    if (firstDoc){
+        console.log(`   - ${collectionName}`);
+        for (const field in firstDoc){
+            console.log(`     - ${field}: ${typeof firstDoc[field]}`);
+        }
+    } else {
+        console.log(`   - ${collectionName} has no data`);
+    }
+};
+    
+
 
 async function main(){
     const url = 'mongodb+srv://Admin:Admin12345@cluster0.0abvsny.mongodb.net/';
@@ -40,6 +61,3 @@ async function main(){
 
 console.log("Starting MongoDB...");
 main().catch(console.error);
-
-// exit
-console.log("End of Program");
