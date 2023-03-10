e2017b7be0f4d6003b74a46ae7810564d9833101
aingez <94661535+aingez@users.noreply.github.com>
2023-03-10 16:00:51 +0700

    create connect,js
    a function for connect to mongo the return client obj

---
+++ backend/src/connect.js
@@ -0,0 +1,20 @@
+// Function to connect to mongoDB database ,
+// Take mongoDB url, then return client object
+
+const {MongoClient} = require('mongodb');
+
+async function mongoConnect(mongoURL){
+    const url = mongoURL;
+    const client = new MongoClient(url);
+    console.log("Connecting to server...");
+    try {
+        await client.connect();
+        console.log("Connected successfully to server!");
+        return client;
+    } catch (e) {
+        console.log("Fail to Connect.");
+        console.error(e);
+        return null;
+    }
+}
+
