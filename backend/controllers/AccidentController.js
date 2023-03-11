e0d10dc329ca557949621ee3bea7b071b06ae3f2
korrawee <korrawee.somyos@gmail.com>
2023-03-12 01:01:06 +0700

    build: add Accident controller

---
+++ backend/controllers/AccidentController.js
@@ -0,0 +1,20 @@
+const AccidentModel = require("../models/mongo/AccidentModel");
+const Accident = new AccidentModel();
+
+class AccidentController {
+  constructor() {
+    console.log("Initializing AccidentController");
+  }
+
+  async getAccidentData(req, res) {
+    const accidents = await Accident.findAll()
+      .catch((e) => {
+        console.error(e);
+        return [];
+      });
+
+    res.send(accidents);
+  }
+}
+
+module.exports = AccidentController;
