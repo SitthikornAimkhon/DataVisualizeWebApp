d34128c0fb35717aeb60873dbb26b2d3fb2a2f63
SitthikornAimkhon <sitthikorn.aim@gmail.com>
2023-03-15 12:03:27 +0700

    Add grid layout

--- data-visualize-webapp/src/App.js
+++ data-visualize-webapp/src/App.js
@@ -3,23 +3,48 @@ import './App.css';
 
 // You can specify which plugins you need here
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 // Import from components file
 import { NavbarApp } from './components/NavbarApp';
+import { Header } from './components/Header';
+import { CardInformation } from './components/CardInformation';
+import { CardWeather } from './components/CardWeather';
 
 function App() {
+  const yearArray = ["No select", "1", "2", "3", "4"];
+  const roadArray = ["No select", "1", "2", "3", "4"];
+
   return (
     <div>
-      <NavbarApp />
+      <NavbarApp YearArray={yearArray} RoadArray={roadArray}/>
       <Container>
         <Row>
           <Col>
-            <h1>1</h1>
+            <Header year="2558" roadName="ศรีรัช" />
           </Col>
-          <Col>
-            <h1>2</h1>
-          </Col>
+        </Row>
+        <Row>
+          <div class="App-container">
+            <div class="item-1">
+              <CardInformation variant="dark" CardTitle="Death" TotalNumber="21" TotalManNumber="13" TotalWomanNumber="12" />
+            </div>
+            <div class="item-2">
+              <CardInformation variant="danger" CardTitle="Injured" TotalNumber="21" TotalManNumber="13" TotalWomanNumber="12" />
+            </div>
+            <div class="item-3">
+              <CardWeather variant="info" CardTitle="Weather" TotalCaseNumber="10" TotalNormalNumber="5" TotalAbnormalNumber="5" />
+            </div>
+            <div class="item-4">
+              {/*for plot graph 1*/}
+            </div>
+            <div class="item-5">
+              {/* for plot graph 2 */}
+            </div>
+            <div class="item-6">
+              {/* for plot graph 3 */}
+            </div>
+          </div>
         </Row>
       </Container>
     </div>
