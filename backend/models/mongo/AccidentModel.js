e4492b6473cfd5045279d3305591eebfece2bffc
korrawee <korrawee.somyos@gmail.com>
2023-03-17 15:19:11 +0700

    build: add date, time, and expw with response

--- backend/models/mongo/AccidentModel.js
+++ backend/models/mongo/AccidentModel.js
@@ -82,6 +82,9 @@ class AccidentModel {
 
     const selectedFields = {
       _id: 1,
+      accident_date: 1,
+      accident_time: 1,
+      expw_step: 1,
       dead_man: 1,
       dead_femel: 1,
     };
@@ -120,141 +123,154 @@ class AccidentModel {
     return deadStat;
   }
 
-  async findAllInjure(expresswayName = null,
-    searchYear = new Date().getFullYear()) {
-      const query = {
-        $expr: {
-          $and: [
-            {
-              $eq: [
-                {
-                  $year: "$accident_date",
-                },
-                searchYear,
-              ],
-            },
-            expresswayName
-              ? {
-                  $eq: ["$expw_step", expresswayName],
-                }
-              : {},
-          ],
-        },
-      };
-  
-      const selectedFields = {
-        _id: 1,
-        injur_femel: 1,
-        injur_man: 1,
-      };
-  
-      const injureResult = await this.Accident.find(query, selectedFields);
-  
-      // If there is no accident
-      if (injureResult.length == 0) {
-        return [];
+  async findAllInjure(
+    expresswayName = null,
+    searchYear = new Date().getFullYear()
+  ) {
+    const query = {
+      $expr: {
+        $and: [
+          {
+            $eq: [
+              {
+                $year: "$accident_date",
+              },
+              searchYear,
+            ],
+          },
+          expresswayName
+            ? {
+                $eq: ["$expw_step", expresswayName],
+              }
+            : {},
+        ],
+      },
+    };
+
+    const selectedFields = {
+      _id: 1,
+      accident_date: 1,
+      accident_time: 1,
+      expw_step: 1,
+      injur_femel: 1,
+      injur_man: 1,
+    };
+
+    const injureResult = await this.Accident.find(query, selectedFields);
+
+    // If there is no accident
+    if (injureResult.length == 0) {
+      return [];
+    }
+
+    return injureResult;
+  }
+
+  async findInjureStat(
+    expresswayName = null,
+    searchYear = new Date().getFullYear()
+  ) {
+    const injureResult = await this.findAllInjure(expresswayName, searchYear);
+
+    let initInjureStat = {
+      total_injure: 0,
+      total_man: 0,
+      total_female: 0,
+    };
+
+    const injureStat = injureResult.reduce((obj, d) => {
+      let tmpObj = { ...obj };
+      tmpObj.total_man += d.injur_man;
+      tmpObj.total_female += d.injur_femel;
+      tmpObj.total_injure = tmpObj.total_female + tmpObj.total_man;
+
+      return tmpObj;
+    }, initInjureStat);
+
+    return injureStat;
+  }
+
+  async findAllWeather(
+    expresswayName = null,
+    searchYear = new Date().getFullYear()
+  ) {
+    const query = {
+      $expr: {
+        $and: [
+          {
+            $eq: [
+              {
+                $year: "$accident_date",
+              },
+              searchYear,
+            ],
+          },
+          expresswayName
+            ? {
+                $eq: ["$expw_step", expresswayName],
+              }
+            : {},
+        ],
+      },
+    };
+
+    const selectedFields = {
+      _id: 1,
+      accident_date: 1,
+      accident_time: 1,
+      expw_step: 1,
+      weather_state: 1,
+    };
+
+    const weatherResult = await this.Accident.find(query, selectedFields);
+
+    // If there is no accident
+    if (weatherResult.length == 0) {
+      return [];
+    }
+
+    return weatherResult;
+  }
+
+  async findWeatherStat(
+    expresswayName = null,
+    searchYear = new Date().getFullYear()
+  ) {
+    const weatherResult = await this.findAllWeather(expresswayName, searchYear);
+
+    let initweatherStat = {
+      total_normal: 0,
+      total_abnormal: 0,
+      rain: 0,
+      heavy_rain: 0,
+      slippy_road: 0,
+    };
+
+    const weatherStat = weatherResult.reduce((obj, d) => {
+      let tmpObj = { ...obj };
+      console.log(d.weather_state);
+      switch (d.weather_state) {
+        case "ฝนตก":
+          tmpObj.rain += 1;
+          break;
+        case "ฝนตกลมแรง":
+          tmpObj.heavy_rain += 1;
+          break;
+        case "ถนนเปียกลื่น":
+          tmpObj.slippy_road += 1;
+          break;
+        default:
+          tmpObj.total_normal += 1;
+          break;
       }
-  
-      return injureResult;
-  }
 
-  async findInjureStat(expresswayName = null,
-    searchYear = new Date().getFullYear()) {
-      
-      const injureResult = await this.findAllInjure(expresswayName, searchYear);
+      tmpObj.total_abnormal =
+        tmpObj.rain + tmpObj.heavy_rain + tmpObj.slippy_road;
 
-      let initInjureStat = {
-        total_injure: 0,
-        total_man: 0,
-        total_female: 0,
-      };
-  
-      const injureStat = injureResult.reduce((obj, d) => {
-        let tmpObj = { ...obj };
-        tmpObj.total_man += d.injur_man;
-        tmpObj.total_female += d.injur_femel;
-        tmpObj.total_injure = tmpObj.total_female + tmpObj.total_man;
-  
-        return tmpObj;
-      }, initInjureStat);
-  
-      return injureStat;
-  }
+      return tmpObj;
+    }, initweatherStat);
 
-  async findAllWeather(expresswayName = null,
-    searchYear = new Date().getFullYear()) {
-      const query = {
-        $expr: {
-          $and: [
-            {
-              $eq: [
-                {
-                  $year: "$accident_date",
-                },
-                searchYear,
-              ],
-            },
-            expresswayName
-              ? {
-                  $eq: ["$expw_step", expresswayName],
-                }
-              : {},
-          ],
-        },
-      };
-  
-      const selectedFields = {
-        _id: 1,
-        weather_state: 1,
-      };
-  
-      const weatherResult = await this.Accident.find(query, selectedFields);
-  
-      // If there is no accident
-      if (weatherResult.length == 0) {
-        return [];
-      }
-  
-      return weatherResult;
-  }
-
-  async findWeatherStat(expresswayName = null,
-    searchYear = new Date().getFullYear()) {
-      
-      const weatherResult = await this.findAllWeather(expresswayName, searchYear);
-
-      let initweatherStat = {
-        total_normal: 0,
-        total_abnormal: 0,
-        rain: 0,
-        heavy_rain: 0,
-        slippy_road: 0,
-      };
-  
-      const weatherStat = weatherResult.reduce((obj, d) => {
-        let tmpObj = { ...obj };
-        console.log(d.weather_state)
-        switch (d.weather_state) {
-          case 'ฝนตก':
-            tmpObj.rain += 1;
-            break;
-          case 'ฝนตกลมแรง':
-            tmpObj.heavy_rain += 1;
-            break;
-          case 'ถนนเปียกลื่น':
-            tmpObj.slippy_road += 1;
-            break;
-          default:
-            tmpObj.total_normal += 1;
-            break;
-        }
-        
-        tmpObj.total_abnormal = tmpObj.rain + tmpObj.heavy_rain + tmpObj.slippy_road;
-  
-        return tmpObj;
-      }, initweatherStat);
-  
-      return weatherStat;
+    return weatherStat;
   }
 }
 
