556e71abcd42adb5828734bff457346a3064555d
SitthikornAimkhon <sitthikorn.aim@gmail.com>
2023-03-17 22:07:56 +0700

    Add function set header

--- data-visualize-webapp/src/components/Header.js
+++ data-visualize-webapp/src/components/Header.js
@@ -2,9 +2,25 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Nav, Navbar, Form, Row, Col } from 'react-bootstrap'
 
 export function Header(props) {
+
+    function SetHeader() {
+        if (props.year!="No select" && props.roadName!="No select") {
+            return (<h3>ข้อมูลปี {props.year} ทางพิเศษ {props.roadName}</h3>);
+        }
+        else if (props.year!="No select" && props.roadName=="No select"){
+            return (<h3>ข้อมูลปี {props.year}</h3>);
+        }
+        else if (props.year=="No select" && props.roadName!="No select") {
+            return (<h3>ข้อมูลทางพิเศษ {props.roadName}</h3>);
+        }
+        else {
+            return (<h3>ข้อมูล</h3>)
+        }
+    }
+    
     return (
         <Col>
-            <h3>ข้อมูลปี {props.year} ทางพิเศษ {props.roadName}</h3>
+            <SetHeader />
         </Col>
     );
 }
