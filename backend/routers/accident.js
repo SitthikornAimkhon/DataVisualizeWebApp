df4601e906f3f9f5a8e516e3444bfa275a800a12
korrawee <korrawee.somyos@gmail.com>
2023-03-17 13:48:50 +0700

    build: add dead and dead-stat APIs (mongo)

--- backend/routers/accident.js
+++ backend/routers/accident.js
@@ -5,6 +5,8 @@ const AccidentController = require('../controllers/AccidentController');
 const accidentController = new AccidentController();
 
 router.get('/', accidentController.getAccidentData);
+router.get('/death', accidentController.findAllDeath);
+router.get('/death/stat', accidentController.getDeadStat);
 router.post('/', accidentController.insertManyAccidents);
 
 module.exports = router;
