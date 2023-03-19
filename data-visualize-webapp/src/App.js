99a73f7aa5c6748a90d234afec66912736621afe
korrawee <korrawee.somyos@gmail.com>
2023-03-19 12:06:56 +0700

    build: integrate fetched data to frontend

--- data-visualize-webapp/src/App.js
+++ data-visualize-webapp/src/App.js
@@ -1,91 +1,174 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 // You can specify which plugins you need here
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import {
+  fetchDeadStat,
+  fetchInjureStat,
+  fetchWeatherStat,
+  findAccidentOnRoad,
+  fetchRoadAvailable,
+  fetchYearAvailable,
+  findAccidentFreq
+} from "./lib/api";
 // Import from components file
-import { NavbarApp } from './components/NavbarApp';
-import { Header } from './components/Header';
-import { CardInformation } from './components/CardInformation';
-import { CardWeather } from './components/CardWeather';
-import { Barchart, Linechart, Piechart } from './components/plot';
+import { NavbarApp } from "./components/NavbarApp";
+import { Header } from "./components/Header";
+import { CardInformation } from "./components/CardInformation";
+import { CardWeather } from "./components/CardWeather";
+import { Barchart, Linechart, Piechart } from "./components/plot";
 
 function App() {
 
-  // Card number
-  const TotalNumberDeath = 21
-  const TotalManNumberDeath = 21
-  const TotalWomanNumberDeath = 12
+  // Declare a new state variable
+  const [yearArray, setYearArray] = useState([]);
+  const [roadArray, setRoadArray] = useState([]);
 
-  const TotalNumberInjured = 34
-  const TotalManNumberInjured = 32
-  const TotalWomanNumberInjured = 3
-
-  const TotalCaseNumber = 34
-  const TotalNormalNumber = 34
-  const TotalAbnormalNumber = 34
-
-  // Declare a new state variable 
-  const yearArray = ["No select", "1", "2", "3", "4"];
-  const roadArray = ["No select", "1", "2", "3", "4"];
-
-  const [yearVal, setYear] = useState(yearArray[0]);
-  const [roadNameVal, setRoadName] = useState(roadArray[0]);
-
-  // Log the value from state variable
-  console.log(yearVal);
-  console.log(roadNameVal);
+  const [yearVal, setYear] = useState('');
+  const [roadNameVal, setRoadName] = useState('');
+  // for dead, injure, weather stats
+  const [deadStat, setDeadStat] = useState({total: 0, man: 0, female: 0});
+  const [injureStat, setInjureStat] = useState({total: 0, man: 0, female: 0});
+  const [weatherStat, setWeatherStat] = useState({total: 0, normal: 0, abmormal: 0});
+  // for pie chart
+  const labels = ["Death", "Injured"];
+  // for bar chart
+  const [barChartData, setbarChartData] = useState({ roadNames: [], counts: [] });
+  // for line chart
+  const [lineChartData, setLineChartData] = useState({times:[], counts:[]});
   
 
-  // Plot graph
-  // for pie chart
-  const labels = ['Death', 'Injured']
-  const values = [50, 12]
-  // for bar chart
-  const locations = ['KMUTNB', 'ศรีรัช', 'วงศ์สว่าง']
-  const count1 = [50, 12, 11]
-  // for line chart
-  const time = ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00']
-  const count2 = [324, 3443, 434, 23, 43, 234, 34, 123]
+  // ========================
+  // Fetch initial data
+  // ========================
+  useEffect(() => {
+    // fetch year available
+    const fetchYears = fetchYearAvailable();
+    fetchYears.then((res) => {
+      setYearArray(res);
+    });
+    // fetch road available
+    const fetchRoads = fetchRoadAvailable();
+    fetchRoads.then((res) => {
+      setRoadArray(res);
+    });
+    
+    // fetch dead stat
+    const resDeadStat = fetchDeadStat();
+    resDeadStat.then((res) => {
+      setDeadStat(res);
+    });
 
-  // useEffect(() => {
-  //   const data = fetch("http://localhost/<uri>").then(async (res) => {
-  //     const json = await res.json();
-  //     return json;
-  //   });
-  // }, [yearVal, roadNameVal]);
+    // fetch injure stat
+    const resInjureStat = fetchInjureStat();
+    resInjureStat.then((res) => {
+      setInjureStat(res);
+    });
+
+    // fetch weather stat
+    const resWeatherStat = fetchWeatherStat();
+    resWeatherStat.then((res) => {
+      setWeatherStat(res);
+    });
+        
+    // fetch accident available on each road
+    const accidents = findAccidentOnRoad();
+    accidents.then((res) => {
+      setbarChartData(res);
+    });
+
+    // fetch accident freq.
+    const resAccidentFreq = findAccidentFreq();
+    resAccidentFreq.then((res)=>{
+      setLineChartData(res);
+    })
+  }, []);
+
+  // Toggle event when year or road changed
+  useEffect(() => {
+    const fetchData = fetchDeadStat(yearVal, roadNameVal);
+    fetchData.then((res) => {
+      setDeadStat(res);
+    });
+
+    // fetch injure stat
+    const resInjureStat = fetchInjureStat(yearVal, roadNameVal);
+    resInjureStat.then((res) => {
+      setInjureStat(res);
+    });
+
+    // fetch weather stat
+    const resWeatherStat = fetchWeatherStat(yearVal, roadNameVal);
+    resWeatherStat.then((res) => {
+      setWeatherStat(res);
+    });
+
+    // fetch accident available on each road
+    const accidents = findAccidentOnRoad(yearVal, roadNameVal);
+    accidents.then((res) => {
+      setbarChartData(res);
+    });
+
+    // fetch accident freq.
+    const resAccidentFreq = findAccidentFreq(yearVal, roadNameVal);
+    resAccidentFreq.then((res)=>{
+      setLineChartData(res);
+    })
+  }, [yearVal, roadNameVal]);
 
   return (
     <div>
-      <NavbarApp YearArray={yearArray} RoadArray={roadArray} setYear={setYear} setRoadName={setRoadName} />
+      <NavbarApp
+        YearArray={yearArray}
+        RoadArray={roadArray}
+        setYear={setYear}
+        setRoadName={setRoadName}
+      />
       <Container>
         <Row>
           <Col>
-            <Header year="2558" roadName="ศรีรัช" />
+            <Header year={yearVal==='' ? 'ทั้งหมด': yearVal} roadName={roadNameVal==='' ? 'ทั้งหมด': roadNameVal} />
           </Col>
         </Row>
         <Row>
           <div class="App-container">
             <div class="item-1">
-              <CardInformation variant="dark" CardTitle="Death" TotalNumber={TotalNumberDeath} TotalManNumber={TotalManNumberDeath} TotalWomanNumber={TotalWomanNumberDeath} />
+              <CardInformation
+                variant="dark"
+                CardTitle="Death"
+                TotalNumber={deadStat.total}
+                TotalManNumber={deadStat.man}
+                TotalWomanNumber={deadStat.female}
+              />
             </div>
             <div class="item-2">
-              <CardInformation variant="danger" CardTitle="Injured" TotalNumber={TotalNumberInjured} TotalManNumber={TotalManNumberInjured} TotalWomanNumber={TotalWomanNumberInjured} />
+              <CardInformation
+                variant="danger"
+                CardTitle="Injured"
+                TotalNumber={injureStat.total}
+                TotalManNumber={injureStat.man}
+                TotalWomanNumber={injureStat.female}
+              />
             </div>
             <div class="item-3">
-              <CardWeather variant="info" CardTitle="Weather" TotalCaseNumber={TotalCaseNumber} TotalNormalNumber={TotalNormalNumber} TotalAbnormalNumber={TotalAbnormalNumber} />
+              <CardWeather
+                variant="info"
+                CardTitle="Weather"
+                TotalCaseNumber={weatherStat.total}
+                TotalNormalNumber={weatherStat.normal}
+                TotalAbnormalNumber={weatherStat.abnormal}
+              />
             </div>
             <div class="item-4">
-              <Linechart times={time} death_counts={count2} />
+              <Linechart times={lineChartData.times} death_counts={lineChartData.counts} />
             </div>
             <div class="item-5">
-              <Barchart locations={locations} death_counts={count1} />
+              <Barchart locations={barChartData.roadNames} death_counts={barChartData.counts} />
             </div>
             <div class="item-6">
-              <Piechart labels={labels} values={values} />
+              <Piechart labels={labels} values={[deadStat.total, injureStat.total]} />
             </div>
           </div>
         </Row>
